6b65b89986fcd7da59dc09ed796de1d6c4c303f3
Donita <mathisdonita@gmail.com>
2018-08-15 22:43:50 -0400

    update

--- js/resources.js
+++ js/resources.js
@@ -5,9 +5,9 @@
  * to load the same image multiple times.
  */
 (function() {
-    const resourceCache = {};
-    const loading = [];
-    const readyCallbacks = [];
+    var resourceCache = {};
+    var loading = [];
+    var readyCallbacks = [];
 
     /* This is the publicly accessible image loading function. It accepts
      * an array of strings pointing to image files or a string for a single
@@ -45,7 +45,7 @@
             /* This URL has not been previously loaded and is not present
              * within our cache; we'll need to load this image.
              */
-            const img = new Image();
+            var img = new Image();
             img.onload = function() {
                 /* Once our image has properly loaded, add it to our cache
                  * so that we can simply return this image if the developer
@@ -82,8 +82,8 @@
      * for loading have in fact been properly loaded.
      */
     function isReady() {
-        let ready = true;
-        for(let k in resourceCache) {
+        var ready = true;
+        for(var k in resourceCache) {
             if(resourceCache.hasOwnProperty(k) &&
                !resourceCache[k]) {
                 ready = false;
