60bc062701ce1f3a032c892d7e84f2d6b6336624
Donita <mathisdonita@gmail.com>
2018-08-15 21:27:17 -0400

    update engine.js

--- js/engine.js
+++ js/engine.js
@@ -9,8 +9,9 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make
- * writing app.js a little simpler to work with.
+ * This engine is available globally via the Engine variable and it also makes
+ * the canvas' context (ctx) object globally available to make writing app.js
+ * a little simpler to work with.
  */
 
 var Engine = (function(global) {
@@ -22,18 +23,7 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime,
-        id;
-
-    const modal = document.querySelector('.modal-bg');
-    const replay = document.querySelector('.modal-button');
-
-    replay.addEventListener('click', function() {
-        modal.classList.toggle('hide');
-        player.reset();
-        player.victory = false;
-        win.requestAnimationFrame(main);
-    });
+        lastTime;
 
     canvas.width = 505;
     canvas.height = 606;
@@ -66,14 +56,7 @@ var Engine = (function(global) {
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
-        if (player.victory === true) {
-            win.cancelAnimationFrame(id);
-            modal.classList.toggle('hide');
-        }
-        else {
-            id = win.requestAnimationFrame(main);
-        }
-
+        win.requestAnimationFrame(main);
     }
 
     /* This function does some initial setup that should only occur once,
@@ -135,9 +118,6 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-
-        // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
 
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
