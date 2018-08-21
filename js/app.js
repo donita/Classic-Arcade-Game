0dc35d813cd33185fc25c54c18fdcc25539d0063
Donita <mathisdonita@gmail.com>
2018-08-21 12:35:21 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,122 +1,140 @@
 // Enemies our player must avoid
-class Enemy  {
-  constructor (x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-  }
+	class Enemy  {
+	  constructor (x, y, speed) {
+	    this.x = x;
+	    this.y = y;
+	    this.speed = speed;
+	    this.sprite = 'images/enemy-bug.png';
+	  }
+	
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt;
+	// Update the enemy's position, required method for game
+	// Parameter: dt, a time delta between ticks
+	update(dt) {
+	    // You should multiply any movement by the dt parameter
+	    // which will ensure the game runs at the same speed for
+	    // all computers.
+	    this.x += this.speed * dt;
+	
 
-    // when off canvas, reset position of enemy to move across again
-    if (this.x > 550) {
-        this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 512);
-    }
+	    // when off canvas, reset position of enemy to move across again
+	    if (this.x > 550) {
+	        this.x = -100;
+	        this.speed = 100 + Math.floor(Math.random() * 512);
+	    }
+	
 
-    // Check for collision between player and enemies
-    if (player.x < this.x + 60 &&
-        player.x + 37 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
-        player.x = 200;
-        player.y = 380;
-    }
-}
+	    // Check for collision between player and enemies
+	    if (player.x < this.x + 60 &&
+	        player.x + 37 > this.x &&
+	        player.y < this.y + 25 &&
+	        30 + player.y > this.y) {
+	        player.x = 200;
+	        player.y = 380;
+	    }
+	}
+	
 
-// Draw the enemy on the screen, required method for game
-render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
-}
+	// Draw the enemy on the screen, required method for game
+	render() {
+	    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	  }
+	}
+	
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+	// Now write your own player class
+	// This class requires an update(), render() and
+	// a handleInput() method.
+	
 
-class Hero {
-  constructor(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/char-boy.png';
-  }
+	class Hero {
+	  constructor(x, y, speed) {
+	    this.x = x;
+	    this.y = y;
+	    this.speed = speed;
+	    this.sprite = 'images/char-boy.png';
+	  }
+	
 
-update() {
-    // Prevent player from moving beyond canvas wall boundaries
-    if (this.y > 380) {
-        this.y = 380;
-    }
+	update() {
+	    // Prevent player from moving beyond canvas wall boundaries
+	    if (this.y > 380) {
+	        this.y = 380;
+	    }
+	
 
-    if (this.x > 400) {
-        this.x = 400;
-    }
+	    if (this.x > 400) {
+	        this.x = 400;
+	    }
+	
 
-    if (this.x < 0) {
-        this.x = 0;
-    }
+	    if (this.x < 0) {
+	        this.x = 0;
+	    }
+	
 
-    // Check for player reaching top of canvas and winning the game
-    if (this.y < 0) {
-        this.x = 200;
-        this.y = 380;
-    }
-}
+	    // Check for player reaching top of canvas and winning the game
+	    if (this.y < 0) {
+	        this.x = 200;
+	        this.y = 380;
+	    }
+	}
+	
 
-render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+	render() {
+	    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+	
 
-handleInput(keyPress) {
-    switch (keyPress) {
-        case 'left':
-            this.x -= this.speed + 60;
-            break;
-        case 'up':
-            this.y -= this.speed + 40;
-            break;
-        case 'right':
-            this.x += this.speed + 60;
-            break;
-        case 'down':
-            this.y += this.speed + 40;
-            break;
-    }
-}
-}
+	handleInput(keyPress) {
+	    switch (keyPress) {
+	        case 'left':
+	            this.x -= this.speed + 60;
+	            break;
+	        case 'up':
+	            this.y -= this.speed + 40;
+	            break;
+	        case 'right':
+	            this.x += this.speed + 60;
+	            break;
+	        case 'down':
+	            this.y += this.speed + 40;
+	            break;
+	    }
+	}
+	}
+	
 
+	
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-let allEnemies = [];
+	// Now instantiate your objects.
+	// Place all enemy objects in an array called allEnemies
+	// Place the player object in a variable called player
+	let allEnemies = [];
+	
 
-// Position "y" where the enemies will are created
-let enemyPosition = [70, 140, 220];
-const player = new Hero(200, 380, 50);
-let enemy;
+	// Position "y" where the enemies will are created
+	let enemyPosition = [60, 140, 220];
+	const player = new Hero(200, 380, 50);
+	let enemy;
+	
 
-enemyPosition.forEach(function(posY) {
-    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
-    allEnemies.push(enemy);
-});
+	enemyPosition.forEach(function(posY) {
+	    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+	    allEnemies.push(enemy);
+	});
+	
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+	// This listens for key presses and sends the keys to your
+	// Player.handleInput() method. You don't need to modify this.
+	document.addEventListener('keyup', function(e) {
+	    var allowedKeys = {
+	        37: 'left',
+	        38: 'up',
+	        39: 'right',
+	        40: 'down'
+	    };
+	
 
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+	    player.handleInput(allowedKeys[e.keyCode]);
+	});
